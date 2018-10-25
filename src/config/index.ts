a3a8a4c40662e7f9caae4fcb7802189e2d915d0b
Guru Prasad G <prasadguru141@gmail.com>
2018-10-25 12:42:44 +0530

    Modified config

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,7 @@ const config: any = {
     MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/cwa',
     SERVER_LOG_DIR: process.env.SERVER_LOG_FILE || '../log',
     JWT_SECRET: process.env.JWT_SECRET || 'no s3cr3t',
-    JWT_PL_SECRET:  process.env.JWT_PL_SECRET || 'my awe = some s3cret',
+    JWT_PL_SECRET:  process.env.JWT_PL_SECRET || 'my am@z!ng s3cr3t',
     JWT_SALT: process.env.JWT_SALT || 'p3pp3r',
     VERSION: process.env.VERSION || 'v1'
 };
